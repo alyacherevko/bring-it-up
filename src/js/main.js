3e3220cdf3b7e19b89613fdd3fb83caa09e9c853
Alya Cherevko <cherevko.alya@gmail.com>
2020-05-23 20:32:36 +0300

    create video player

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 import Slider from "./modules/slider";
+import PlayVideo from "./modules/playVideo";
 
 window.addEventListener("DOMContentLoaded", () => {
   const slider = new Slider(".page", ".next");
   slider.render();
+
+  const player = new PlayVideo(".showup .play", ".overlay");
+  player.init();
 });
